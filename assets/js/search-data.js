a1f0b9b19a7f853c60c6a7e27d3eeda5fd705b35
tianyi0216 <77199099+tianyi0216@users.noreply.github.com>
2025-09-27 01:50:53 +0000

    Deploying to gh-pages from @ tianyi0216/tianyi0216.github.io@7f7b9be52c01198b5faeaaf488d6429bb5716f8b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,20 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
-        },{id: "nav-submenus",
-          title: "submenus",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/_pages/dropdown/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
