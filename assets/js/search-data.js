f567e05e44050f97e31d96048d554bf0211eed8d
tianyi0216 <77199099+tianyi0216@users.noreply.github.com>
2026-01-15 03:23:46 +0000

    Deploying to gh-pages from @ tianyi0216/tianyi0216.github.io@4c203193574215931fe97c71a3a2d1fbf7b4447d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -494,7 +494,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=", "_blank");
+          window.open("https://scholar.google.com/citations?user=VdMuVfIAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
